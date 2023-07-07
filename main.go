8f139fbef288efd74d91cd57b8d500d7323c3713
Medet Ahmetson <admin@blocklords.io>
2023-07-07 13:18:31 +0300

    main function in the main package uses the independent package

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"github.com/Seascape-Foundation/sds-service-lib/configuration"
+	"github.com/Seascape-Foundation/sds-service-lib/controller"
+	"github.com/Seascape-Foundation/sds-service-lib/independent"
 	"github.com/Seascape-Foundation/sds-service-lib/log"
+	"github.com/Seascape-Foundation/static-seascape-service/handler"
 )
 
 func main() {
@@ -16,19 +19,58 @@ func main() {
 		logger.Fatal("log.New(`main`)", "error", err)
 	}
 
-	logger.Info("Load app configuration begin...")
+	logger.Info("load app configuration begin...")
 	appConfig, err := configuration.NewAppConfig(logger)
 	if err != nil {
 		logger.Fatal("configuration.NewAppConfig", "error", err)
 	}
-	logger.Info("Load app configuration end!")
+	logger.Info("load app configuration end!")
+
+	//
+	// Validations
+	//
+	if len(appConfig.Services) == 0 {
+		logger.Fatal("no seascape.yml or the yaml file doesn't contain services")
+	}
+
+	logger.Info("for quick development, we assume the seascape.yml has one controller, service and extension")
+	serviceConfig := appConfig.Services[0]
+	if serviceConfig.Type != configuration.IndependentType {
+		logger.Fatal("first service in seascape.yml is not an independent type", "type", serviceConfig.Type)
+	}
+	if len(serviceConfig.Controllers) == 0 {
+		logger.Fatal("first service in seascape.yml doesn't contain controllers")
+	}
+	controllerConfig := serviceConfig.Controllers[0]
+	if controllerConfig.Type != configuration.ReplierType {
+		logger.Fatal("the first controller is not a replier",
+			"service name", serviceConfig.Name,
+			"controller name", controllerConfig.Name,
+			"controller type", controllerConfig.Type,
+		)
+	}
 
 	/////////////////////////////////////////////////////////////////////////
 	//
-	// Run the Core services:
+	// Create the controller
 	//
 	/////////////////////////////////////////////////////////////////////////
 
-	// Start the core services
-	go Run(appConfig)
+	replier, err := controller.NewReplier(logger)
+	if err != nil {
+		logger.Fatal("failed to create a controller", "error", err)
+	}
+	// add to replier the commands
+	handler.RegisterCommands(replier)
+
+	service, err := independent.New(appConfig.Services[0])
+	if err != nil {
+		logger.Fatal("failed to create an independent service", "error", err)
+	}
+	err = service.AddController("main", replier)
+	if err != nil {
+		logger.Fatal("failed to add controller into the independent service", "error", err)
+	}
+
+	service.Run()
 }
